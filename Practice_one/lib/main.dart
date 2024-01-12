27316761468d5b3a4b81fde8567b8b08943af0ee
Tayba Rahman Khan <93123747+201014002@users.noreply.github.com>
2024-01-12 00:42:35 -0800

    Updated commit

--- Practice_one/lib/main.dart
+++ Practice_one/lib/main.dart
@@ -6,65 +6,57 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Home(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-    
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-   
-        title: Text(widget.title),
+        backgroundColor: Colors.amber,
+        title: Text('My App'),
       ),
-      body: Center(
-   
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      body: ListView(
+        children: [
+          ListTile(
+            leading: Icon(Icons.star),
+            title: Text('Item 1'),
+          ),
+          ListTile(
+            leading: Icon(Icons.star),
+            title: Text('Item 2'),
+          ),
+          ListTile(
+            leading: Icon(Icons.star),
+            title: Text('Item 3'),
+          ),
+          ListTile(
+            leading: Icon(Icons.star),
+            title: Text('Item 4'),
+          ),
+          ListTile(
+            leading: Icon(Icons.star),
+            title: Text('Item 5'),
+          ),
+          ListTile(
+            leading: Icon(Icons.star),
+            title: Text('Item 6'),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
